001a8b885df5f6761e7881e3207a179db7a4d8d4
GustiRecovery <gusti.degen@gmail.com>
2026-03-23 23:09:21 +0100

    removed bug

--- NewClass.cs
+++ NewClass.cs
@@ -4,10 +4,9 @@ public class Class1
 {
 	public Class1()
 	{
-<<<<<<< HEAD
-		void a() { int a; }
-=======
-		Console.WriteLine("aaaa");
->>>>>>> a2046c15021d71196fab5df409c48ba05c470779
+		void a() { 
+			int a;
+			Console.WriteLine("aaaa");
+		}
 	}
 }
